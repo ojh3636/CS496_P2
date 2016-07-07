af49df9120a6744681e35d293370ae67819bb52c
yummyummyummy <hihoosilver@kaist.ac.kr>
2016-07-07 09:54:28 +0900

    added tabB

---
+++ app/src/main/java/com/project/cs496/tab_app/GridItem.java
@@ -0,0 +1,18 @@
+package com.project.cs496.tab_app;
+
+public class GridItem {
+    private String image;
+
+    public GridItem() {
+        super();
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+}
